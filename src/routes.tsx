a866b8d88423e4959778595980d4defee07f1b69
Bruno <brunooeduardoop@gmail.com>
2020-05-23 09:11:41 -0300

    Modificou routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,15 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Homepage from './pages/Homepage/';
 import Navbar from './components/Navbar';
+
 export default function Routes() {
   return (
     <>
       <Route path={['/inicial', '/biblioteca']} component={Navbar} />
       <Switch>
         <Route path="/inicial" exact component={Homepage} />
-        <Route path="/" exact component={Login} />
-        <Route path="/biblioteca" exact component={Login} />
-        <Route path="/" component={Login} />
+        <Route path="/login" exact component={Login} />
       </Switch>
     </>
   );
