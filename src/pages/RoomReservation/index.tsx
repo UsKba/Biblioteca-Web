15ca055de9e0ff9c68cd2b19f13a2b24244034a4
NovaDrake76 <NovaDrake76@gmail.com>
2020-05-24 17:53:41 -0300

    modificou tela reserva e a inicial >> >> >> Co-authored-by: Fellpz9 <zanathann@gmail.com> >> Co-authored-by: BrunooEduardooP <brunooeduardoop@gmail.com>

--- src/pages/RoomReservation/index.tsx
+++ src/pages/RoomReservation/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import {
+import { 
   Container,
   DatePicker,
   DateInterval,
   RoomList,
   Line,
   Rectangle,
+  StudentName,
+  Weeks,
+  Week,
+  WeekName
 } from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -22,51 +26,110 @@ const RoomReservation: React.FC = () => {
         <DateInterval>25/04 - 29/05</DateInterval>
         <FontAwesomeIcon icon={faChevronRight} size="2x" color="#509" />
       </DatePicker>
+      <Weeks>
+        <Week>
+        <WeekName>Segunda</WeekName>
+        </Week>
+       
+        <Week>
+        <WeekName active={true}>Terça</WeekName>
+        </Week>
+       
+        <Week>
+        <WeekName>Quarta</WeekName>
+        </Week>
+       
+        <Week>
+        <WeekName>Quinta</WeekName>
+        </Week>
+       
+        <Week>
+        <WeekName>Sexta</WeekName>
+        </Week>
+       
 
+        </Weeks>      
       <RoomList>
         <Line>
-          <Rectangle backgroundColor="blue"></Rectangle>
-          <Rectangle backgroundColor="blue"> F1-3</Rectangle>
-          <Rectangle backgroundColor="blue"> F1-4</Rectangle>
-          <Rectangle backgroundColor="blue"> F1-5</Rectangle>
-          <Rectangle backgroundColor="blue"> F1-6</Rectangle>
+          <Rectangle backgroundColor="#007bff"></Rectangle>
+          <Rectangle backgroundColor="#007bff">F1-3</Rectangle>
+          <Rectangle backgroundColor="#007bff">F1-4</Rectangle>
+          <Rectangle backgroundColor="#007bff">F1-5</Rectangle>
+          <Rectangle backgroundColor="#007bff">F1-6</Rectangle>
         </Line>
 
         <Line>
-          <Rectangle>07:15 - 08:00</Rectangle>
-          <Rectangle></Rectangle>
-          <Rectangle></Rectangle>
-          <Rectangle></Rectangle>
-          <Rectangle></Rectangle>
-        </Line>
+          <Rectangle backgroundColor="#007bff">07:15 - 08:00</Rectangle>
+          <Rectangle >
 
-        <Line>
-          <Rectangle>08:00 - 09:00</Rectangle>
-          <Rectangle></Rectangle>
+          </Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
         </Line>
 
         <Line>
-          <Rectangle>09:00 - 10:00</Rectangle>
-          <Rectangle></Rectangle>
+          <Rectangle backgroundColor="#007bff">08:00 - 09:00</Rectangle>
+          <Rectangle backgroundColor="#007bff">
+            <StudentName>Nathan</StudentName>
+            <StudentName>José Eduardo</StudentName>
+            <StudentName>Halyson</StudentName>
+
+          </Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
         </Line>
 
         <Line>
-          <Rectangle>10:00 - 11:00</Rectangle>
+          <Rectangle backgroundColor="#007bff">09:00 - 10:00</Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
+          <Rectangle backgroundColor="#F55">
+            <StudentName>Idaslon</StudentName>
+            <StudentName>Carlos Eduardo</StudentName>
+            <StudentName>Bruno Eduardo</StudentName>
+            <StudentName>João Victor</StudentName>
+            <StudentName>Sebas Tião</StudentName>
+            <StudentName>Lucas</StudentName>
+            <StudentName>Caio</StudentName>
+          </Rectangle>
+          <Rectangle></Rectangle>
+        </Line>
+
+        <Line>
+          <Rectangle backgroundColor="#007bff">10:00 - 11:00</Rectangle>
+          <Rectangle backgroundColor="#F55">
+            <StudentName>Natália</StudentName>
+            <StudentName>João Eduardo</StudentName>
+            <StudentName>Júlio</StudentName>
+            <StudentName>César</StudentName>
+            <StudentName>Artur</StudentName>
+            <StudentName>Arthur</StudentName>
+            <StudentName>George</StudentName>
+
+          </Rectangle>
+          <Rectangle backgroundColor="#F55">
+            <StudentName>Haline</StudentName>
+            <StudentName>Haludilson</StudentName>
+            <StudentName>Haleber</StudentName>
+          </Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
         </Line>
 
         <Line>
-          <Rectangle>11:00 - 12:00</Rectangle>
-          <Rectangle></Rectangle>
+          <Rectangle backgroundColor="#007bff">11:00 - 12:00</Rectangle>
+          <Rectangle backgroundColor="#F55">
+            <StudentName>Nathan</StudentName>
+            <StudentName>José Eduardo</StudentName>
+            <StudentName>Halysete</StudentName>
+            <StudentName>Halynildo</StudentName>
+            <StudentName>Halyne</StudentName>
+            <StudentName>Halex</StudentName>
+            <StudentName>Halynildo</StudentName>
+
+          </Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
--- src/pages/RoomReservation/styles.ts
+++ src/pages/RoomReservation/styles.ts
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: #fff;
   width: 100%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
 `;
 
 export const DatePicker = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  padding: 20px;
+  width: 90%;
+  align-self: center;
+  background-color: #fff;
+  margin-top: 20px;
+  padding: 10px;
 `;
 
 export const DateInterval = styled.h1`
@@ -21,11 +27,22 @@ export const DateInterval = styled.h1`
 export const RoomList = styled.div`
   display: flex;
   flex-direction: column;
+  flex: 1;
+  width: 90%;
+  align-self: center;
+  border-radius: 10px;
+  background-color: #fff;
 `;
 
 export const Line = styled.div`
   display: flex;
   flex-direction: row;
+  flex: 1 1;
+
+  &:first {
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
 `;
 
 interface IRectangle {
@@ -34,9 +51,55 @@ interface IRectangle {
 
 export const Rectangle = styled.div<IRectangle>`
   display: flex;
-  flex-grow: 1;
-  width: 150px;
-  height: 50px;
-  border: 1px solid #000;
+  flex-direction: column;
+  flex: 1 1;
+  padding: 15px;
+  min-height: 100px;
+  border: 0px solid rgba(0, 0, 0, 0.1);
+  border-bottom-width: 1px;
+  border-left-width: 1px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: none;
+  justify-content: center;
+  align-items: center;
   background-color: ${(props) => props.backgroundColor};
+`;
+
+export const StudentName = styled.span`
+  color: #fff;
+  font-size: 16px;
+  font-weight: lighter;
+  cursor: pointer;
+`;
+
+export const Weeks = styled.div`
+  width: 90%;
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  background-color: #fff;
+`;
+
+interface IWeekName {
+  active?: boolean;
+}
+
+export const Week = styled.div`
+  padding: 15px;
+  flex: 1 1;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const WeekName = styled.span<IWeekName>`
+  cursor: pointer;
+
+  color: ${(props) => (props.active ? '#007BFF' : '#333')};
+  ${(props) => props.active && 'border-bottom: 5px solid #007bff'};
+  ${(props) => props.active && 'font-weight: bold'};
+
+  padding: 5px 15px;
+  font-size: 18px;
 `;
