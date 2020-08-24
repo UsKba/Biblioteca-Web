b5114657bf88203edad32fb47cf74197dba32389
Fellpz9 <zanathann@gmail.com>
2020-08-24 15:52:02 -0300

    URI de redirecionamento modificado

--- src/services/suap.js
+++ src/services/suap.js
@@ -3,8 +3,8 @@ const CLIENT_ID = 'RhbGlTFhHHN0c9TlqzhXHWYoK2jenc5wWK9cCyuB';
 let REDIRECT_URI = 'http://localhost:3000/home';
 const SUAP_URL = 'https://suap.ifrn.edu.br';
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-  REDIRECT_URI = 'https://mystifying-kirch-1890cf.netlify.app';
+if (process.env.NODE_ENV === 'production') {
+  REDIRECT_URI = 'https://mystifying-kirch-1890cf.netlify.app/home';
 }
 
 const suap = new SuapClient(SUAP_URL, CLIENT_ID, REDIRECT_URI);
