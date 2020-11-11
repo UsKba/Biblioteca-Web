ea3d194642bb589634a34d01372b23338777eca8
Bruno Eduardo de Souza Medeiros <brunooeduardoop@gmail.com>
2020-11-11 10:21:46 -0300

    Updated suap.js redirect_uri when production mode to new netlify url
    Co-authored-by: NovaDrake76 <nathan.araujo@academico.ifrn.edu.br>
    Co-authored-by: Fellpz9 <zanathann@gmail.com>

--- src/services/suap.js
+++ src/services/suap.js
@@ -4,7 +4,7 @@ let REDIRECT_URI = 'http://localhost:3000/';
 const SUAP_URL = 'https://suap.ifrn.edu.br';
 
 if (process.env.NODE_ENV === 'production') {
-  REDIRECT_URI = 'https://mystifying-kirch-1890cf.netlify.app/';
+  REDIRECT_URI = 'https://smartlibrary.netlify.app/';
 }
 
 const suap = new SuapClient(SUAP_URL, CLIENT_ID, REDIRECT_URI);
