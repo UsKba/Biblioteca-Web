b1d389bbb7553da23b6015f51803873a05bc3d8f
Fellpz9 <zanathann@gmail.com>
2021-02-04 10:59:38 -0300

    progresso na tabela de salas(nao terminado)
    Co-authored-by: DestroyeerU <destroyeeru18@gmail.com>

--- src/pages/Rooms/index.tsx
+++ src/pages/Rooms/index.tsx
@@ -1,26 +1,17 @@
-import React, { useCallback } from 'react';
+/* eslint-disable no-alert */
+import React, { useCallback, useEffect, useState } from 'react';
 import { BsPlus } from 'react-icons/bs';
 import { useHistory, Switch, Route, Link } from 'react-router-dom';
 
+import { getRequest } from '~/utils/api';
+import getFirstDayOfWeek from '~/utils/firstDayOfWeek';
+
 import colors from '~/styles/colors';
+
+import { Reserve } from '~/types/';
 
 import DateList from './components/DateList';
 import {
-  // Container,
-  // TableContainer,
-  // Title,
-  // TitleContainer,
-  // TopSide,
-  // BottomSide,
-  // ScheduleList,
-  // ScheduleButton,
-  // Rectangle,
-  // CurrentDayContainer,
-  // CurrentDay,
-  // CurrentMonth,
-  // StyledLink,
-  // RentButton,
-
   Container,
   TableTopInformation,
   Table,
@@ -38,6 +29,7 @@ const Rooms: React.FC = () => {
   const formatter = new Intl.DateTimeFormat('pt-br', { month: 'long' });
   const reserveDate = new Date();
   const monthFormatted = formatter.format(reserveDate);
+  const [reserves, setReserves] = useState([] as Reserve[]);
 
   const changePeriod = useCallback(
     (periodValue) => {
@@ -56,13 +48,49 @@ const Rooms: React.FC = () => {
     [history]
   );
 
+  useEffect(() => {
+    async function getReserves() {
+      const sunday = getFirstDayOfWeek();
+      const monday = new Date(sunday.getFullYear(), sunday.getMonth(), sunday.getDate() + 1);
+      const friday = new Date(sunday.getFullYear(), sunday.getMonth(), sunday.getDate() + 5);
+
+      const startDate = `${monday.getFullYear()}/${monday.getMonth()}/${monday.getDate()}`;
+      const endDate = `${friday.getFullYear()}/${friday.getMonth()}/${friday.getDate()}`;
+
+      const { data, error } = await getRequest<Reserve[]>('reserves/all', {
+        params: {
+          startDate,
+          endDate,
+        },
+      });
+
+      if (error || !data) {
+        alert('Erro ao listar as reservas');
+        return;
+      }
+
+      const mondayReserves = data.filter((reserve) => {
+        const dateOfReserve = new Date(reserve.date);
+
+        if (dateOfReserve.getUTCDay() === 1) {
+          return true;
+        }
+        return false;
+      });
+
+      console.log(mondayReserves);
+      console.log(data);
+      setReserves(data || []);
+    }
+
+    getReserves();
+  }, []);
+
   return (
     <Container>
       <TableTopInformation>
         <Dropdown onChange={(event) => changePeriod(event.target.value)}>
-          <option value="1" selected>
-            Manhã
-          </option>
+          <option value="1">Manhã</option>
           <option value="2">Tarde</option>
           <option value="3">Noite</option>
         </Dropdown>
@@ -75,85 +103,27 @@ const Rooms: React.FC = () => {
       <Table>
         <Switch>
           <Route path="/salas-manha" exact>
-            <TableColumn>
-              <RoomTitle>F1-3</RoomTitle>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>7:15 - 8:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>8:00 - 9:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>9:00 - 10:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>10:00 - 11:00</RoomCardHour>
-              </RoomCard>
-            </TableColumn>
-
-            <TableColumn>
-              <RoomTitle>F1-4</RoomTitle>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>7:15 - 8:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>8:00 - 9:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>9:00 - 10:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>10:00 - 11:00</RoomCardHour>
-              </RoomCard>
-            </TableColumn>
-
-            <TableColumn>
-              <RoomTitle>F1-5</RoomTitle>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>7:15 - 8:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>8:00 - 9:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>9:00 - 10:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>10:00 - 11:00</RoomCardHour>
-              </RoomCard>
-            </TableColumn>
-
-            <TableColumn removeBorder>
-              <RoomTitle>F1-6</RoomTitle>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>7:15 - 8:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>8:00 - 9:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>9:00 - 10:00</RoomCardHour>
-              </RoomCard>
-              <RoomCard>
-                <BsPlus />
-                <RoomCardHour>10:00 - 11:00</RoomCardHour>
-              </RoomCard>
-            </TableColumn>
+            {reserves.map((reserve) => (
+              <TableColumn key={reserve.id}>
+                <RoomTitle>F1-3</RoomTitle>
+                <RoomCard>
+                  <BsPlus />
+                  <RoomCardHour>7:15 - 8:00</RoomCardHour>
+                </RoomCard>
+                <RoomCard>
+                  <BsPlus />
+                  <RoomCardHour>8:00 - 9:00</RoomCardHour>
+                </RoomCard>
+                <RoomCard>
+                  <BsPlus />
+                  <RoomCardHour>9:00 - 10:00</RoomCardHour>
+                </RoomCard>
+                <RoomCard>
+                  <BsPlus />
+                  <RoomCardHour>10:00 - 11:00</RoomCardHour>
+                </RoomCard>
+              </TableColumn>
+            ))}
           </Route>
 
           <Route path="/salas-tarde" exact>
@@ -321,146 +291,6 @@ const Rooms: React.FC = () => {
         </Switch>
       </Table>
     </Container>
-    // <Container>
-    //   <TitleContainer>
-    //     <Title>Salas</Title>
-    //     <StyledLink to="/reservar">
-    //       <RentButton>Agendar sala</RentButton>
-    //     </StyledLink>
-    //   </TitleContainer>
-    //   <TableContainer>
-    //     <TopSide>
-    //       <CurrentDayContainer>
-    //         <CurrentDay>{reserveDate.getDate()}</CurrentDay>
-    //         <CurrentMonth>{monthFormatted}</CurrentMonth>
-    //       </CurrentDayContainer>
-    //       <DateList />
-    //       <ScheduleList>
-    //         <ScheduleButton onClick={() => history.push('/salas-manha')} active={pathname === '/salas-manha'}>
-    //           Manhã
-    //         </ScheduleButton>
-    //         <ScheduleButton onClick={() => history.push('/salas-tarde')} active={pathname === '/salas-tarde'}>
-    //           Tarde
-    //         </ScheduleButton>
-    //         <ScheduleButton onClick={() => history.push('/salas-noite')} active={pathname === '/salas-noite'}>
-    //           Noite
-    //         </ScheduleButton>
-    //       </ScheduleList>
-    //     </TopSide>
-
-    //     <BottomSide>
-    //       <Switch>
-    //         <Route path="/salas-manha" exact>
-    //           <Rectangle backgroundColor={colors.primary}>Salas e Horários</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-3</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-4</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-5</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-6</Rectangle>
-
-    //           <Rectangle backgroundColor={colors.primary}>7:15 - 8:00</Rectangle>
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[3]} />
-
-    //           <Rectangle backgroundColor={colors.primary}>8:00 - 9:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[0]} />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-
-    //           <Rectangle backgroundColor={colors.primary}>9:00 - 10:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-
-    //           <Rectangle backgroundColor={colors.primary}>10:00 - 11:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-
-    //           <Rectangle backgroundColor={colors.primary}>11:00 - 12:00</Rectangle>
-    //           <Rectangle borderColor={colors.roomStatus[3]} />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[3]} />
-    //         </Route>
-
-    //         <Route path="/salas-tarde" exact>
-    //           <Rectangle backgroundColor={colors.primary}>Salas e Horários</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-3</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-4</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-5</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-6</Rectangle>
-
-    //           <Rectangle backgroundColor={colors.primary}>13:15 - 14:00</Rectangle>
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[3]} />
-
-    //           <Rectangle backgroundColor={colors.primary}>14:00 - 15:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[0]} />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-
-    //           <Rectangle backgroundColor={colors.primary}>15:00 - 16:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-
-    //           <Rectangle backgroundColor={colors.primary}>16:00 - 17:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-
-    //           <Rectangle backgroundColor={colors.primary}>17:00 - 18:00</Rectangle>
-    //           <Rectangle borderColor={colors.roomStatus[3]} />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[3]} />
-    //         </Route>
-
-    //         <Route path="/salas-noite" exact>
-    //           <Rectangle backgroundColor={colors.primary}>Salas e Horários</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-3</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-4</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-5</Rectangle>
-    //           <Rectangle backgroundColor={colors.primary}> F1-6</Rectangle>
-
-    //           <Rectangle backgroundColor={colors.primary}>18:15 - 19:00</Rectangle>
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[3]} />
-
-    //           <Rectangle backgroundColor={colors.primary}>19:00 - 20:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[0]} />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-
-    //           <Rectangle backgroundColor={colors.primary}>20:00 - 21:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-
-    //           <Rectangle backgroundColor={colors.primary}>21:00 - 22:00</Rectangle>
-    //           <Rectangle />
-    //           <Rectangle />
-    //           <Rectangle borderColor={colors.roomStatus[2]} />
-    //           <Rectangle />
-    //         </Route>
-    //       </Switch>
-    //     </BottomSide>
-    //   </TableContainer>
-    // </Container>
   );
 };
 
